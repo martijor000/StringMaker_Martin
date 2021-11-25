d26c767ec57a18c2bc788a01b690ef7aecdb5fc6
martijor000 <82613327+martijor000@users.noreply.github.com>
2021-11-25 00:57:26 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringMaker_Martin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringManager sm = new StringManager();
+            Console.WriteLine(sm.ToString());
+            Console.WriteLine(sm.Reverse("Sunbeam Tiger"));
+            Console.WriteLine(sm.Equals("Sunbeam Tiger"));
+            Console.WriteLine(sm.Symmetric("ABBA"));
+            Console.WriteLine(sm.Symmetric("ABA"));
+            Console.WriteLine(sm.Symmetric("ABba"));
+
+
+
+        }
+    }
+}
---
+++ StringManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringMaker_Martin
+{
+    class StringManager
+    {
+        Stack<char> stringStack = new Stack<char>();
+
+
+        public StringManager()
+        {
+            
+        }
+
+        public string Reverse(string s)
+        {
+            char[] test = s.ToCharArray();
+            return StackString(test);
+        }
+
+        public string Reverse(string s, bool b)
+        {
+            char[] test = s.ToCharArray();
+            return StackString(test);
+        }
+
+        public bool Symmetric(string s)
+        {
+            if(s == Reverse(s))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            string test = "Dan";
+            int ASCII = 0;
+
+            foreach(char c in test)
+            {
+                ASCII += (int)c;
+            }
+            return ASCII.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj is string)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string StackString(char[] charArray)
+        {
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                stringStack.Push(charArray[i]);
+            }
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                charArray[i] = stringStack.Pop();
+            }
+            string stri = new string(charArray);
+
+            return stri;
+        }
+
+        private string CapitalizeString(string cap)
+        {
+            char[] charA = cap.ToLower().ToCharArray();
+            for (int i = 0; i < charA.Length; i++)
+            {
+                if(charA[i] == 0)
+                {
+                    charA[i] = charA[i];
+                }
+            }
+                return "";
+        }
+    }
+}
